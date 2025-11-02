4e8b78003c6db71be9bdd6f14108d1549aac49bd
CodeByVignesh <vigneshkumar.selvaraj27@gmail.com>
2025-11-03 00:18:50 +0100

    Refactored the code

--- query_db.py
+++ query_db.py
@@ -1,14 +1,14 @@
 from langchain_experimental.sql import SQLDatabaseChain
 
-def query_db(db_connection, model):
+def query_db_func(db_connection, model, question):
 
     if db_connection is None:
         print("Database connection is not available.")
-         return None
+        return None
      
     try:
         db_chain = SQLDatabaseChain.from_llm(llm=model, db=db_connection, verbose=True)
-        answer1 = db_chain.run("How many t-shirts do we have left for nike in extra small size and white color?")
+        answer1 = db_chain.run(question)
         print(answer1)
         return answer1
     except Exception as e:
